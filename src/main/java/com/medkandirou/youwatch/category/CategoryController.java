48a01f06b59bf4ddc753733382fbad8dcc93c414
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2024-01-17 14:58:19 +0100

    update category

--- src/main/java/com/medkandirou/youwatch/category/CategoryController.java
+++ src/main/java/com/medkandirou/youwatch/category/CategoryController.java
@@ -13,34 +13,34 @@ import java.util.List;
 @RequestMapping(path="api/categorie")
 public class CategoryController {
 
-    private final CategorieService CategorieService;
+    private final CategoryService categoryService;
 
-    private CategoryController(CategorieService CategorieService){
-        this.CategorieService=CategorieService;
+    private CategoryController(CategoryService categoryService){
+        this.categoryService=categoryService;
     }
 
     @GetMapping
     public ResponseEntity<List<CategoryDTOres>> getAll(){
-        return new ResponseEntity<>(CategorieService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<CategoryDTOres> save(@Valid @RequestBody CategoryDTOreq categorieId){
-        return new ResponseEntity<>(CategorieService.save(categorieId), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.save(categorieId), HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<CategoryDTOres> update(@Valid @RequestBody CategoryDTOreq categorieId){
-        return new ResponseEntity<>(CategorieService.save(categorieId), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.save(categorieId), HttpStatus.OK);
     }
 
     @GetMapping(path = {"{categorieId}"})
     public ResponseEntity<CategoryDTOres> findById(@PathVariable("categorieId") Long categorieId){
-        return new ResponseEntity<>(CategorieService.findById(categorieId), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.findById(categorieId), HttpStatus.OK);
     }
 
     @DeleteMapping(path = {"{categorieId}"})
     public ResponseEntity<CategoryDTOreq> deleteById(@PathVariable("categorieId") Long categorieId){
-        return new ResponseEntity<>(CategorieService.deleteById(categorieId), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.deleteById(categorieId), HttpStatus.OK);
     }
 }
