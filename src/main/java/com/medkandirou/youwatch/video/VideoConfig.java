45f72029669b6551f67c0d7341376d615d62b063
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2024-03-27 11:13:42 +0000

    youcode a toute

--- src/main/java/com/medkandirou/youwatch/video/VideoConfig.java
+++ src/main/java/com/medkandirou/youwatch/video/VideoConfig.java
@@ -27,18 +27,16 @@ public class VideoConfig {
             Category c2 = new Category("Gaming");
             Category c3 = new Category("Education");
             Category c4 = new Category("Cooking");
-            Category c5 = new Category("Technology");
             Category c6 = new Category("Sports");
             Category c7 = new Category("Fashion");
             Category c8 = new Category("Travel");
-            Category c9 = new Category("Health & Fitness");
             Category c10 = new Category("Comedy");
-            categoryRepository.saveAll(List.of(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10));
+            categoryRepository.saveAll(List.of(c1,c2,c3,c4,c6,c7,c8,c10));
 
 
 
 
-            RegisterRequest ch1 = new RegisterRequest("John", "Doe", "user@gmail.com", "password", Role.USER);
+            RegisterRequest ch1 = new RegisterRequest("Med", "Kandirou", "user@gmail.com", "password", Role.USER);
             RegisterRequest ch2 = new RegisterRequest("Jane", "Smith", "admin@gmail.com", "password",Role.ADMIN);
             RegisterRequest ch3 = new RegisterRequest("Alice", "Johnson", "alice@gmail.com", "password",Role.USER);
             RegisterRequest ch4 = new RegisterRequest("Bob", "Brown", "bob@gmail.com", "password",Role.USER);
@@ -52,29 +50,25 @@ public class VideoConfig {
 
             Channel channel1= new Channel();
             channel1.setId(1L);
-            Video video1 = new Video("title 1", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
+            Channel channel3= new Channel();
+            channel3.setId(3L);
+            Video video1 = new Video("Leo messi destoy Real Madrid 2017", "Match 2017 , barcelone vs real madrid in league spain ", "http://127.0.0.1:5000/uploads/fe8868bf-9ac7-4eea-8fe5-e9fc8cf8eaee.mp4", "http://127.0.0.1:5000/uploads/9057fa7b-071d-4c8d-8e35-5d19107a0868.jpg" ,LocalDateTime.now(),100000, 10000,c6,channel3);
+            Video video2 = new Video("Youcode school promo 2022/2024 ", "Le numérique et les nouvelles technologies sont partout autour de nous, mais leurs codes et leurs langages ne sont pas toujours naturels et innés pour tous.C'est pourquoi YouCode veut mettre le code entre toutes les mains pour réduire les inégalités", "http://127.0.0.1:5000/uploads/7409da24-ad41-44e0-8303-ec2ede937194.mp4", "http://127.0.0.1:5000/uploads/4594dc1c-905d-4c94-80f4-9d5671aeaf50.jpg", LocalDateTime.now(), 50000,5000,c3,channel1);
+            Video video3 = new Video("Fist match of brahim diaz with morrocco", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
                     "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg" ,LocalDateTime.now(),100000, 10000,c1,channel1);
-            Video video2 = new Video("title 2", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
+                    "        labore wes anderson cred nesciunt sapiente ea proident.", "http://127.0.0.1:5000/uploads/f65d79cb-4cd3-41ea-86d3-aeaaef9325e4.mp4", "http://127.0.0.1:5000/uploads/ee106420-d3c3-41f4-9157-97f854774ac5.jpg", LocalDateTime.now(), 99000,78000,c6,channel1);
+            Video video4 = new Video("Coding in real life", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
                     "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 50000,5000,c1,channel1);
-            Video video3 = new Video("title 3", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
+                    "        labore wes anderson cred nesciunt sapiente ea proident.", "http://127.0.0.1:5000/uploads/91616489-0b46-4edc-9c5f-92ebef90a5b3.mp4", "http://127.0.0.1:5000/uploads/e4f108a3-cab0-47ab-bab2-310cf7c3b404.jpg", LocalDateTime.now(), 93000,100,c10,channel3);
+            Video video5 = new Video("Algerie vs cameron (but ekambi)", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
                     "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 99000,78000,c2,channel1);
-            Video video4 = new Video("title 4", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
-                    "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 93000,100,c2,channel1);
-            Video video5 = new Video("title 4", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
-                    "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 93000,100,c3,channel1);
+                    "        labore wes anderson cred nesciunt sapiente ea proident.", "http://127.0.0.1:5000/uploads/82c01bbc-91de-491a-aee7-2529e15be6da.mp4", "http://127.0.0.1:5000/uploads/44397a5d-b51e-4a18-807d-c151b45d2ce6.jpg", LocalDateTime.now(), 93000,100,c10,channel3);
 
-            Video video6 = new Video("title 4", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
+            Video video6 = new Video("Match with friends ( sun 95 vs java ventures )", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
                     "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 93000,100,c3,channel1);
-            Video video7 = new Video("title 4", "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\n" +
-                    "        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer\n" +
-                    "        labore wes anderson cred nesciunt sapiente ea proident.", "https://player.cloudinary.com/embed/?cloud_name=drlfmis63&public_id=samples/dance-2", "https://flowbite.com/docs/images/people/profile-picture-3.jpg", LocalDateTime.now(), 93000,100,c4,channel1);
-            videoRepository.saveAll(List.of(video1,video2,video3,video4,video5,video6,video7));
+                    "        labore wes anderson cred nesciunt sapiente ea proident.", "http://127.0.0.1:5000/uploads/50e71e68-e66c-4770-b4f1-0602b18b2f94.mp4", "http://127.0.0.1:5000/uploads/7d2b3b3b-c938-42ab-a8cc-0d9dd0492c21.jpg", LocalDateTime.now(), 93000,100,c6,channel3);
+
+            videoRepository.saveAll(List.of(video1,video2,video3,video4,video5,video6));
         };
     }
 }
